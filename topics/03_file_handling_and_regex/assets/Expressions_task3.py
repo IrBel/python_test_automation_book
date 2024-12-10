05883d46f8de71d07475a3d785fef286e23b6973
IrBel <47665970+IrBel@users.noreply.github.com>
2024-12-10 18:03:58 +0100

    Expressions_task3.py
    Regular Expressions
     
    3. Define if the string contains at least one Upper case letter followed by Lower case letters. E.g. '75serS3' - False; '75WseTrS3' - True;

---
+++ topics/03_file_handling_and_regex/assets/Expressions_task3.py
@@ -0,0 +1,10 @@
+def define_uppercase_followed_by_lowercase(check_str):
+    n = range(len(check_str) - 1)
+    for i in n:
+        if check_str[i].isupper() and check_str[i + 1].islower():
+            return True
+    return False
+
+# Example
+print(define_uppercase_followed_by_lowercase('75serS3'))
+print(define_uppercase_followed_by_lowercase('75WseTrS3'))
